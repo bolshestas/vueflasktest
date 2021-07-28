c1a59b3e28891c8433f3115a8877550d14dac91a
Bolshakov Stanislav <bolshestas@gmail.com>
2021-07-28 18:12:34 +0300

    fullproject

---
+++ vueflasktest/frontend/client/src/main.js
@@ -0,0 +1,14 @@
+import BootstrapVue from 'bootstrap-vue';
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+
+
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount('#app');
